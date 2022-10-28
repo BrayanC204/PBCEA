108421ec0e8a89e315b02fd97abf8425195936ff
BRAYAN RAMIREZ VARGAS <84393631+BrayanC204@users.noreply.github.com>
2022-10-28 12:39:47 -0500

    Agregado de pantallas al Stack

--- plantbook/App.js
+++ plantbook/App.js
@@ -3,6 +3,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator} from '@react-navigation/stack';
 
 import Login from './src/screens/Login/Login';
+import Contacto from './src/screens/Contactanos/Contactanos';
+import Detalle from './src/screens/Detalle_planta/Detalle_planta';
+import Favoritos from './src/screens/Favoritos/Favoritos';
+import Principal from './src/screens/Principal/Principal';
+import Registro from './src/screens/Registro/Registro';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +22,36 @@ export default function App() {
           } } }
             name= 'Login'
             component={Login} />
+            <Stack.Screen
+          options = {{ headerStyle: {
+            backgroundColor: '#ffffff'
+          } } }
+            name= 'Contacto'
+            component={Contacto} />
+            <Stack.Screen
+          options = {{ headerStyle: {
+            backgroundColor: '#ffffff'
+          } } }
+            name= 'Detalle'
+            component={Detalle} />
+            <Stack.Screen
+          options = {{ headerStyle: {
+            backgroundColor: '#ffffff'
+          } } }
+            name= 'Favoritos'
+            component={Favoritos} />
+            <Stack.Screen
+          options = {{ headerStyle: {
+            backgroundColor: '#ffffff'
+          } } }
+            name= 'Principal'
+            component={Principal} />
+            <Stack.Screen
+          options = {{ headerStyle: {
+            backgroundColor: '#ffffff'
+          } } }
+            name= 'Registro'
+            component={Registro} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ plantbook/src/screens/Registro/Registro.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+const Registro = (props) =>{
+    return(
+        <View>
+            <Text>Hola</Text>
+        </View>
+    )
+}
+
+export default Registro;

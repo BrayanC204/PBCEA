77aeaa6cd16af1169c7b6be9366b6ac1c18c56b4
BRAYAN RAMIREZ VARGAS <84393631+BrayanC204@users.noreply.github.com>
2022-10-28 12:23:20 -0500

    Creacion de pantallas

--- plantbook/App.js
+++ plantbook/App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+
+import Login from './src/screens/Login/Login';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Hola mundo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = 'Login'>
+        <Stack.Screen
+          options = {{ headerStyle: {
+            backgroundColor: '#ffffff'
+          } } }
+            name= 'Login'
+            component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ plantbook/src/screens/Login/Login.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+const Login = (props) =>{
+    return(
+        <View>
+            <Text>Hola</Text>
+        </View>
+    )
+}
+
+export default Login;

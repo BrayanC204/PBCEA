a92b6a11b592a4bb8ba4cb4c1709a3144ff04f48
BRAYAN RAMIREZ VARGAS <84393631+BrayanC204@users.noreply.github.com>
2022-11-15 14:47:57 -0600

    Funcionalidad de Login y Registro, sin estilos

--- plantbook/src/screens/Login/Login.js
+++ plantbook/src/screens/Login/Login.js
@@ -1,13 +1,150 @@
 import React, { useState } from 'react';
 import {
     View,
+    Image,
     Text,
+    TextInput,
+    Span,
+    TouchableOpacity,
+    Alert,
 } from 'react-native';
+import firebase from '../../database/firebase';
+import getError from '../../helpers/errores_es_mx';
+import LoginStyles from './Login.forms';
 
 const Login = (props) =>{
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [btnVisible, setBtnVisible] = useState(true);
+
+    const validaLogin = async () => {
+
+        let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+        if (email.length < 1 || password.length < 1) {
+            Alert.alert('ERROR','Campos vacios', [
+                {
+                    text: 'Rellenar los campos por favor',
+                    onPress: () => setEmail(''),
+                },
+            ]);
+            return;
+        }
+
+        if (!emailRegex.test(email)) {
+            Alert.alert('ERROR','Correo Invalido', [
+                {
+                    text: 'Corregir',
+                    onPress: () => setEmail(''),
+                },
+            ]);
+            return;
+        }
+        
+        setBtnVisible(false);
+        
+        try {
+            const userFirebase = await firebase.auth.signInWithEmailAndPassword(
+                email,
+                password
+            );
+
+            Alert.alert(
+                'Bienvenido',
+                `${userFirebase.user.email}`,
+                [
+                    {
+                        text: 'Acceder',
+                        onPress: () => {
+                            setBtnVisible(true);
+                            props.navigation.navigate(
+                                'Principal'
+                            );
+                        },
+                    },
+                ]
+            );
+        } catch (e) {
+            Alert.alert('ERROR', getError(e.code), [
+                {
+                    text: 'Volver a intentar',
+                    onPress: () => {
+                        setBtnVisible(true);
+                    },
+                },
+            ]);
+        }
+    }
     return(
         <View>
-            <Text>Hola</Text>
+            <View>
+                <Image
+                    source={require('../../../assets/logo.png')}
+                    />
+            </View>
+
+            <View>
+                <Text>Iniciar Sesion</Text>
+            </View>
+
+            <View>
+                <Text>E-mail</Text>
+                <TextInput
+                    placeholder='Correo Electronico'
+                    keyboardType='email-address'
+                    maxLength={50}
+                    autoCapitalize='none'
+                    onChangeText={(val) => setEmail(val)}
+                    value={email}
+                    require
+                    />
+            </View>
+
+            <View>
+                <Text>Contraseña</Text>
+                <TextInput
+                    placeholder='Contraseña'
+                    keyboardType='default'
+                    maxLength={20}
+                    secureTextEntry
+                    onChangeText={(val) => setPassword(val)}
+                    value={password}
+                    require
+                    />
+            </View>
+
+            <View
+                style={{
+                    display: btnVisible ? 'flex' : 'none'
+                    }}
+                    >
+                <Text
+                    onPress={validaLogin}>
+                        Iniciar Sesion
+                </Text>
+            </View>
+
+            <View>
+                <Text>¿No tienes una cuenta? Registrate Aqui</Text>
+                <TouchableOpacity 
+                    onPress={() => {props.navigation.navigate('Registro')
+                    }}>
+                    <Text>
+                        Registrate
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
+            <View>
+            <TouchableOpacity 
+                    onPress={() => {props.navigation.navigate('Contacto')
+                    }}>
+                    <Text>
+                        Contactanos
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
--- plantbook/src/screens/Registro/Registro.js
+++ plantbook/src/screens/Registro/Registro.js
@@ -1,15 +1,263 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import {
     View,
     Text,
+    Alert,
+    ScrollView,
+    Image,
+    TextInput,
+    ActivityIndicator,
 } from 'react-native';
+import firebase from '../../database/firebase';
 
-const Registro = (props) =>{
+export default class Registro extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            surname: '',
+            surname2: '',
+            email: '',
+            password: '',
+            confirmPass: '',
+            aiVisible: false,
+            btnVisible: true,
+        }
+    }
+
+    validaRegistro(state) {
+        Alert.alert('Hola ${name}');
+    }
+
+    render() {
+        const validaRegistro = async () => {
+            let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+            this.setState({
+				aiVisible: true,
+				btnVisible: false,
+			});
+            
+            if((this.state.name.length < 1) ||
+            (this.state.surname.length < 1) ||
+            (this.state.surname2.length < 1) ||
+            (this.state.email.length < 1) ||
+            (this.state.password.length < 1) ||
+            (this.state.confirmPass.length < 1)) {
+                Alert.alert('ERROR', 'Rellene todos los campos',
+                [
+                    {
+                        text: 'Corregir',
+                        onPress: () => {
+                            this.setState({
+                                aiVisible: false,
+                                btnVisible: true,
+                            });
+                            return;
+                        }
+                    }
+                ])
+            }
+
+            if (!emailRegex.test(this.state.email)) {
+                Alert.alert('ERROR','Correo Invalido', [
+                    {
+                        text: 'Corregir',
+                        onPress: () => {
+                            this.setState({
+                                email: '',
+                                aiVisible: false,
+                                btnVisible: true,
+                            });
+                            return;
+                        }
+                    },
+                ]);
+            }
+
+            if (!(this.state.password === this.state.confirmPass)){
+                Alert.alert('ERROR','Contraseñas diferentes', [
+                    {
+                        text: 'Corregir',
+                        onPress: () => {
+                            this.setState({
+                                password: '',
+                                confirmPass: '',
+                                aiVisible: false,
+                                btnVisible: true,
+                            });
+                            return;
+                        }
+                    },
+                ]);
+            }
+
+            try {
+                const usuarioFirebase = await firebase.auth.createUserWithEmailAndPassword(
+                    this.state.email,
+                    this.state.password,
+                );
+                if (usuarioFirebase) {
+                    const addusu = await firebase.db.collection('USUARIOS').add({
+                        id : usuarioFirebase.user.uid,
+                        name : this.state.name,
+                        surname : this.state.surname,
+                        surname2 : this.state.surname2,
+                        email : this.state.email,
+                        user_type : "usuario",
+                    });
+                    Alert.alert(
+                        'Registro Exitoso',
+                        `Gracias por Registrarte ${this.state.name}\n Tu ID:\n${addusu.id}`,
+                        [
+                            {
+                                text: 'Continuar',
+                                onPress: () => {
+                                    this.setState({
+                                        aiVisible: false,
+                                        btnVisible: true,
+                                        name: '',
+                                        surname: '',
+                                        surname2: '',
+                                        email: '',
+                                        password: '',
+                                        confirmPass: '',
+                                    });
+                                },
+                            },
+                        ],
+                    );
+                }
+            }catch (e) {
+                Alert.alert('ERROR', e.toString(), [
+                    {
+                        text: 'Aceptar',
+                        onPress: () => {
+                            this.setState({
+                                aiVisible: false,
+                                btnVisible: true,
+                            });
+                        },
+                    },
+                ],
+                { cancelable: false });
+            }
+        }
+
     return(
-        <View>
-            <Text>Hola</Text>
-        </View>
+        <ScrollView>
+            <View>
+                <Image
+                    source={require('../../../assets/logo.png')}
+                    />
+            </View>
+            <View>
+                <Text>Registrate</Text>
+                <Text>Llena este formulario para registrarte</Text>
+            </View>
+            <View>
+                <TextInput
+                    placeholder='Nombre'
+                    keyboardType='default'
+                    onChangeText={(val) => {
+                        this.setState({
+                            name: val,
+                        });
+                    }}
+                    value={this.state.name}
+                    />
+            </View>
+            <View>
+                <TextInput
+                    placeholder='Apellido Paterno'
+                    keyboardType='default'
+                    onChangeText={(val) => {
+                        this.setState({
+                            surname: val,
+                        });
+                    }}
+                    value={this.state.surname}
+                    />
+            </View>
+            <View>
+                <TextInput
+                    placeholder='Apellido Materno'
+                    keyboardType='default'
+                    onChangeText={(val) => {
+                        this.setState({
+                            surname2: val,
+                        });
+                    }}
+                    value={this.state.surname2}
+                    />
+            </View>
+            <View>
+                <TextInput
+                 placeholder='Email'
+                 keyboardType='email-address'
+                 autoCapitalize='none'
+                 onChangeText={(val) => {
+                    this.setState({
+                        email: val,
+                    });
+                }}
+                 value={this.state.email}
+                 />
+            </View>
+            <View>
+                <TextInput
+                    placeholder='Contraseña (8 digitos)'
+                    keyboardType='default'
+                    secureTextEntry
+                    onChangeText={(val) => {
+                        this.setState({
+                            password: val,
+                        });
+                    }}
+                    value={this.state.password}
+                    maxLength={10}
+                />
+            </View>
+            <View>
+                <TextInput
+                    placeholder='Confirma tu contraseña'
+                    keyboardType='default'
+                    secureTextEntry
+                    onChangeText={(val) => {
+                        this.setState({
+                            confirmPass: val,
+                        });
+                    }}
+                    value={this.state.confirmPass}
+                    maxLength={10}
+                />
+            </View>
+            <ActivityIndicator
+                size='large'
+                color='#000'
+                style={{
+                    marginVertical: 15,
+                    display: this.state.aiVisible
+                        ? 'flex'
+                        : 'none',
+                }}
+            />
+            <View
+                style={{
+                    display: this.state.btnVisible
+                        ? 'flex'
+                        : 'none'
+                }}
+            >
+                <Text
+                    onPress={validaRegistro}
+                >
+                    Registrarme
+                </Text>
+            </View>
+        </ScrollView>
     )
+    }
 }
-
-export default Registro;

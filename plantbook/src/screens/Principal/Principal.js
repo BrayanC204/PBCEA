4d3b29489105d8b3de16d1f7e1153aec900fa820
BRAYAN RAMIREZ VARGAS <84393631+BrayanC204@users.noreply.github.com>
2022-11-23 18:59:09 -0600

    API instalada

--- plantbook/src/screens/Principal/Principal.js
+++ plantbook/src/screens/Principal/Principal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     View,
     Text,
@@ -6,10 +6,22 @@ import {
     ImageBackground,
     Image,
     Button,
-    StyleSheet
+    StyleSheet,
+    TextInput
 } from 'react-native';
+import firebase from '../../database/firebase';
 import Options from '../Options/Options';
+
 const Principal = (props) =>{
+    const [plantas, setPlantas] = useState([]);
+    const url = 'http://dtai.uteq.edu.mx/~critre192/Apis/Plantas';
+    
+        useEffect(() => {
+            fetch(url)
+            .then(response => response.json())
+            .then(data => setPlantas(data))
+        }, [url]);
+
     return(
         <ScrollView>
             <Text style={{
@@ -28,6 +40,13 @@ const Principal = (props) =>{
                     <View style={styles.container}>
                         <Image source={require('../../../assets/plantasfondo.jpg')} style={styles.imageStyl}  />
                         <Text style={styles.userStyle}>Aqui encontraras informacion sobre todas las plantas</Text>
+                    </View>
+                    <View>
+                        {
+                            Object.values(plantas).map((val) =>
+                            <Image source={{uri: val.imagen_url}} style={styles.imageStyl}/>
+                            )
+                        }
                     </View>
                     
         </ScrollView>
@@ -52,6 +71,7 @@ const styles= StyleSheet.create({
     height:200,
     alignItems: 'center',
     justifyContent:'center',
+    marginBottom: 4,
   },
     userStyle:{
         position:'absolute',

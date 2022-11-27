7dfc14adb0272e9740013221865ab04800662335
BRAYAN RAMIREZ VARGAS <84393631+BrayanC204@users.noreply.github.com>
2022-11-27 17:09:49 -0600

    Modal a Pantalla principal

--- plantbook/src/screens/Principal/Principal.js
+++ plantbook/src/screens/Principal/Principal.js
@@ -7,15 +7,58 @@ import {
     Image,
     Button,
     StyleSheet,
-    TextInput
+    TextInput,
+    Modal,
+    Alert,
+    Pressable,
 } from 'react-native';
 import firebase from '../../database/firebase';
 import Options from '../Options/Options';
-import Detalle from '../Detalle_planta/Detalle_planta';
 
 const Principal = (props) =>{
     const [plantas, setPlantas] = useState([]);
     const url = 'http://dtai.uteq.edu.mx/~critre192/Apis/Plantas';
+    const [modalVisible, setModalVisible] = useState(false);
+    const [id, setId] = useState("");
+    const [imagen_url, setImagen_url] = useState("");
+    const [nombre, setNombre] = useState("");
+    const [nombre_cientifico, setNombre_cientifico] = useState("");
+    const [clima, setClima] = useState("");
+    const [descripcion, setDescripcion] = useState("");
+
+    function verModal(){
+        if(modalVisible){
+            return(
+                <Modal
+                    animationType='slide'
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");
+                        setModalVisible(!modalVisible);
+                    }}>
+                        <View style={styles.centeredView}>
+                            <View style={styles.modalView}>
+                                    <ScrollView>
+                                        <Image source={{uri: imagen_url}} style={styles.imageStyl2}/>
+                                        <Text>Nombre: {nombre}</Text>
+                                        <Text>Nombre Cientifico: {nombre_cientifico}</Text>
+                                        <Text>Descripcion: {descripcion}</Text>
+                                        <Text>Clima: {clima}</Text>
+                                        </ScrollView>
+                                            <Pressable
+                                                style={[styles.button, styles.buttonClose]}
+                                                onPress={() => {
+                                                    setModalVisible(!modalVisible)
+                                                }}>
+                                                    <Text>Cerrar</Text>
+                                            </Pressable>
+                                        </View>
+                                    </View>
+                            </Modal>
+            );
+        }
+    }
     
         useEffect(() => {
             fetch(url)
@@ -59,14 +102,19 @@ const Principal = (props) =>{
 						marginHorizontal: 80,
 	                    textAlign: 'center',
                         fontSize: 15}}
-						onPress={() => {props.navigation.navigate(Detalle);
-                            }}
+						onPress={() => {[setModalVisible(true),
+                            setId(val.id),
+                            setImagen_url(val.imagen_url),
+                            setNombre(val.nombre),
+                            setNombre_cientifico(val.nombre_cientifico),
+                            setClima(val.clima),
+                            setDescripcion(val.descripcion)]}}
 		            > Ver + </Text>
                             </View>
                             )
                         }
                     </View>
-                    
+                    {verModal(modalVisible)}
         </ScrollView>
     )
 }
@@ -136,6 +184,36 @@ const styles= StyleSheet.create({
         alignItems:'center',
         justifyContent:'center',
     },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 30,
+        padding: 40,
+        alignItems: 'baseline',
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+      },
+      button: {
+        borderRadius: 20,
+        padding: 10,
+        paddingHorizontal: 100,
+        elevation: 2,
+      },
+      buttonClose: {
+        backgroundColor: "#2196F3",
+      },
 });
 
 export default Principal;
